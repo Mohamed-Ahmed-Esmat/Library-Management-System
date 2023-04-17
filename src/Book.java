661de69850d6f1c3f20e2bc8add763bb15ca733c
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-04-17 21:34:51 +0200

    count added

--- src/Book.java
+++ src/Book.java
@@ -1,17 +1,21 @@
 public class Book {
     private String title;
     private boolean is_available;
+    private static int count=0;
     private Book[] books= new Book[10]; 
     public Book() {
+        count++;
     }
 
     public Book(String title) {
         this.title = title;
+        count++;
     }
 
     public Book(String title, boolean is_available) {
         this.title = title;
         this.is_available = is_available;
+        count++;
     }
 
     public String getTitle() {
