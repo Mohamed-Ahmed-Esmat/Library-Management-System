082556a99dc5a9b81821264621f6eebf66bf0a26
Youssef Mohamed <126986279+youssefmohammed03@users.noreply.github.com>
2023-04-18 20:57:54 +0200

    setters for user done

--- src/User.java
+++ src/User.java
@@ -31,30 +31,57 @@ public class User {
         return id;
     }
 
+    public void setId(String id){
+        this.id = id;
+    }
+
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password){
+        this.password = password;
+    }
     
 
     public String getFirstName() {
         return firstName;
     }
 
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
     }
 
     public String getAddress() {
         return address;
     }
 
+    public void setAddress(String address){
+        this.address = address;
+    }
+
     public String getCellPhone() {
         return cellPhone;
     }
 
+    public void setCellPhone(String cellPhone ){
+        this.cellPhone = cellPhone;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail() {
+        this.email = email;
     }
 
     public boolean isBlocked() {
@@ -71,11 +98,6 @@ public class User {
 
     public void setType(String type) {
         this.type = type;
-    }
-
-    public void user_search(int id){
-
-
     }
 
     private static int getCount() {
